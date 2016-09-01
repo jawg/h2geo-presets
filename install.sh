3e15e97b3bd0f4686058d37ab2f3cbb1cf22569d
loic ortola <contact@loicortola.com>
2016-09-01 23:49:15 +0200

    Fixed permission

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy."
+    exit 0
+fi
+
+openssl aes-256-cbc -K $encrypted_20aefe02d3a5_key -iv $encrypted_20aefe02d3a5_iv -in deploy_key.enc -out deploy_key -d
